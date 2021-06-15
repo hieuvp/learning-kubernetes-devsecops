26c13c7a1b6113fde298ff541b3c3358465cf10b
Harrison (Hieu) Van <Hieu_Van@mckinsey.com>
2021-06-15 09:49:42 +0700

    install-sonarqube

--- scripts/install-sonarqube.sh
+++ scripts/install-sonarqube.sh
@@ -5,4 +5,12 @@ set -eoux pipefail
 # minikube stop
 # minikube delete
 
-minikube start --driver=virtualbox
+# minikube start --driver=virtualbox
+
+cd sonarqube/charts/sonarqube
+helm dependency update
+kubectl create namespace sonarqube
+helm upgrade --install \
+  --values=values.yaml \
+  --namespace=sonarqube \
+  sonarqube ./
